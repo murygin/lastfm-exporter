8588672aace2ab0da95444efaf1f5994b1a01b1a
Daniel Murygin <daniel.murygin@gmail.com>
2013-03-17 20:49:16 +0100

    This is a project to test couchbase. Initial commit.

---
+++ src/de/murygin/couchbase/ArtistExportThread.java
@@ -0,0 +1,109 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Daniel Murygin.
+ *
+ * This program is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public License 
+ * as published by the Free Software Foundation, either version 3 
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Daniel Murygin <dm[at]sernet[dot]de> - initial API and implementation
+ ******************************************************************************/
+package de.murygin.couchbase;
+
+import org.apache.log4j.Logger;
+
+import com.couchbase.client.CouchbaseClient;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+
+/**
+ *
+ *
+ * @author Daniel Murygin <dm[at]sernet[dot]de>
+ */
+public class ArtistExportThread extends Thread {
+    
+    private static final Logger LOG = Logger.getLogger(ArtistExportThread.class);
+    
+    Client jerseyClient = null;
+    CouchbaseClient couchbaseClient = null;
+    String artistName = null;
+    
+    
+    public ArtistExportThread(Client jerseyClient, CouchbaseClient cb, String username) {
+        super();
+        this.jerseyClient = jerseyClient;
+        this.couchbaseClient = cb;
+        this.artistName = username;
+    }
+    
+    /* (non-Javadoc)
+     * @see java.lang.Thread#run()
+     */
+    @Override
+    public void run() {
+        saveRecentTracksOfFriends(artistName);
+    }
+    
+    private void saveRecentTracksOfFriends(String artistName) {                
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Loading info of artist: " + artistName + " from lastfm...");
+        }
+        String jsonResponse = getArtistInfoAsJson(artistName);         
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("lastfm JSON response: " + jsonResponse);
+        } 
+        saveJsonInCouchbase(artistName, jsonResponse);   
+        if (LOG.isInfoEnabled()) {
+            LOG.info("Info of " + artistName + " saved in couchbase.");
+        }
+    }
+    
+    private String getArtistInfoAsJson(String artistName) {
+        String url = buildWebserviceUrl(artistName);
+        
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Webservice URL: " + url);
+        }
+        
+        WebResource webResource = jerseyClient.resource(url);
+        String jsonResponse = webResource.get(String.class);
+        return jsonResponse;
+    }
+
+    private void saveJsonInCouchbase(String artistName, String jsonResponse) {
+        try {
+            String key = buildDocumentKey(artistName);            
+            couchbaseClient.set(key, 0, jsonResponse);
+        } catch (Exception e) {
+            System.err.println("Error connecting to Couchbase: " + e.getMessage());
+        }
+    }
+    
+    private String buildWebserviceUrl(String artistName) {
+        artistName = artistName.replaceAll(" ", "%20");
+        StringBuilder sb = new StringBuilder();
+        sb.append("http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=");
+        sb.append(artistName);
+        sb.append("&api_key=");
+        sb.append(LastfmExporter.key);
+        sb.append("&format=json");     
+        return sb.toString();
+    }
+    
+    private String buildDocumentKey(String artistName) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("lastfm:artist:");
+        sb.append(artistName);
+        return sb.toString();
+    }
+}
---
+++ src/de/murygin/couchbase/LastfmExporter.java
@@ -0,0 +1,113 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Daniel Murygin.
+ *
+ * This program is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public License 
+ * as published by the Free Software Foundation, either version 3 
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Daniel Murygin <dm[at]sernet[dot]de> - initial API and implementation
+ ******************************************************************************/
+package de.murygin.couchbase;
+
+import java.net.URI;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.log4j.Logger;
+
+import com.couchbase.client.CouchbaseClient;
+import com.sun.jersey.api.client.Client;
+
+import de.umass.lastfm.Artist;
+
+/**
+ *
+ *
+ * @author Daniel Murygin <dm[at]sernet[dot]de>
+ */
+public class LastfmExporter {
+
+    private static final Logger LOG = Logger.getLogger(LastfmExporter.class);
+    
+    public static String key = "040f7b24f14afd8cd5b6385fceba24f7"; //this is the key used in the last.fm API examples online.
+   
+    private static String artistNameToStart = "Eminem";
+    
+    private static int maxNumberOfThreads = 10;
+    
+    private ExecutorService taskExecutor;
+    
+    Client jerseyClient;
+    CouchbaseClient cb = null;
+    
+    public static Set<String> processedUser = new HashSet<String>();
+    public static Set<String> processedArtists= new HashSet<String>();
+
+    public LastfmExporter() {
+        super();
+        jerseyClient = Client.create();
+        List<URI> uris = new LinkedList<URI>();
+        uris.add(URI.create("http://127.0.0.1:8091/pools"));
+
+        try {
+            cb = new CouchbaseClient(uris, "lastfm", "");
+        } catch (Exception e) {
+            System.err.println("Error connecting to Couchbase: " + e.getMessage());
+        }
+        
+        taskExecutor = Executors.newFixedThreadPool(maxNumberOfThreads);
+    }
+
+    public static void main(String[] args) {   
+        LastfmExporter loader = new LastfmExporter();    
+        loader.saveArtistInfo(artistNameToStart);    
+    }
+    
+    private void saveArtistInfo(String artistName) {
+        if(processedArtists.contains(artistName)) {
+            return;
+        } else {
+            processedArtists.add(artistName);
+        }
+        
+        Collection<Artist> artistCollection = Artist.getSimilar(artistName, key);
+        
+        if (LOG.isInfoEnabled()) {
+            LOG.info("");
+            LOG.info("");
+            LOG.info("Processing artist " + artistName + ", number of similar:  " + artistCollection.size());
+        }
+        
+        // export artist
+        ArtistExportThread thread = new ArtistExportThread(jerseyClient, cb, artistName);
+        taskExecutor.execute(thread);
+        
+       // export similar artists
+        for (Artist artist : artistCollection) {
+            String currentArtist = artist.getName(); 
+            
+            ArtistExportThread similarArtistThread = new ArtistExportThread(jerseyClient, cb, currentArtist);
+            taskExecutor.execute(similarArtistThread);
+         
+        }
+        for (Artist artist : artistCollection) {
+            saveArtistInfo(artist.getName());
+        }
+    }
+
+}
